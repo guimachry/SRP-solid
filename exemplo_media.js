67999ffedb39531f0459722cb1c98e4b05e970c3
lucas <usecrispin@gmail.com>
2024-09-04 19:39:08 -0300

    exemplo_media

---
+++ exemplo_media.js
@@ -0,0 +1,13 @@
+class avaliacoes {
+    constructor(trabalho, prova, participacao){
+        this.trabalho = trabalho;
+        this.prova = prova;
+        this.participacao = participacao;
+    }
+}
+
+class desempenho {
+    get media(){
+        return parseInt((trabalho, prova, participacao)/3)
+    }
+}
