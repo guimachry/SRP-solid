07cb97aef3a5ac12aef4c34c4c037e1ab727cf76
MauroPellizzaro <mauropellizzaro123@gmail.com>
2024-09-04 15:50:43 -0300

    adição de um código sem srp

---
+++ exemplo_sem.java
@@ -0,0 +1,24 @@
+namespace Lab.Principles.Solid
+{
+    public class Livro
+    {
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Texto { get; set; }
+
+        public string SubstituirPalavraNoTexto(string palavra)
+        {
+            return Texto.Replace(palavra, Texto);
+        }
+
+        public bool PalavraEstáNoTexto(string palavra)
+        {
+            return Texto.Contains(palavra);
+        }
+
+        public void ImprimirTextoNoConsole(string texto)
+        {
+            Console.WriteLine($"Nome do livro: {texto}");
+        }
+    }
+}
